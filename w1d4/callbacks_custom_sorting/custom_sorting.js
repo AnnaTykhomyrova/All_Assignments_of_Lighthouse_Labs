468d5dc53375ef9c44acd0a51eb348c8bf6776d3
AnnaTykhomyrova <tykhomyrovaanna@gmail.com>
2019-05-31 05:06:45 +0000

    using sort() method in the arra

---
+++ w1d4/callbacks_custom_sorting/custom_sorting.js
@@ -0,0 +1,33 @@
+var students = [{
+    id: 1,
+    name: "bruce",
+    age: 40
+  },
+  {
+    id: 2,
+    name: "zoidberg",
+    age: 22
+  },
+  {
+    id: 3,
+    name: "alex",
+    age: 22
+  },
+  {
+    id: 4,
+    name: "alex",
+    age: 30
+  },
+];
+
+// console.log(students);
+students.sort((objA, objB) => {
+  if (objA.name === objB.name){
+    return  (objA.age < objB.age);
+  } else {
+    return (objA.name > objB.name);
+  }
+
+});
+
+console.log(students);
